de60a04fb98bda2b27d5a993a83a4a1434e83d02
LaxmiRathore <rathorelaxmi.rathore@gmail.com>
2017-10-23 15:58:15 +0530

    this is my third commit

--- src/com/emids/main/HelperClass.java
+++ src/com/emids/main/HelperClass.java
@@ -11,43 +11,43 @@ public class HelperClass {
 		while (condition) {
 			System.out.println("please Enter your choice " + "\n" + "1)To view all books" + " \n"
 					+ "2)to search books by bookname " + "\n" + "3)search by auther name" + "\n" + "4)add to cart"
-					+ "\n" + "5)to order book directly" + "\n" + "6)exit" + "\n" + "7)Admin");
+					+ "\n" + "5)to order book directly" + "\n" + "6)Admin" + "\n" + "7)exit");
 			int choice = scanner.nextInt();
-			switch (choice) {
-			case 1:
-				shop.all();
-				break;
-			case 2:
-				shop.searchBybookName();
-				break;
-			case 3:
-				shop.searchByautherName();
-				break;
-			case 4:
-				shop.addToCart();
-
-				order1.order();
-				break;
-			case 5:
-				Book book1 = order1.getBookdirectly();
-				Order.buyProducts(book1);
-				break;
-
-			case 6:
-				System.out.println("thank you ....");
-				condition = false;
-				System.exit(0);
-			case 7:
-				Admin admin = new Admin();
-				boolean valid = admin.getvalidation();
-				if (valid == true) {
-					admin.facilites();
-					break;
-				}
-			default:
-				System.exit(0);
-			}
+			condition = methodofSwitch(shop, order1, condition, choice);
 			System.out.println("\n");
+		}
+		return condition;
+	}
+	private static boolean methodofSwitch(OnlineBookShop shop, Order order1, boolean condition, int choice) {
+		switch (choice) {
+		case 1:
+			shop.all();
+			break;
+		case 2:
+			shop.searchBybookName();
+			break;
+		case 3:
+			shop.searchByautherName();
+			break;
+		case 4:
+			shop.addToCart();
+			order1.order();
+			break;
+		case 5:
+			Book book1 = order1.getBookdirectly();
+			Order.buyProducts(book1);
+			break;
+		case 6:
+			Admin admin = new Admin();
+			boolean valid = admin.getvalidation();
+			if (valid == true) {
+				admin.facilites();
+				break;
+			}
+			default:
+			System.out.println("thank you ....");
+			condition = false;
+			System.exit(0);
 		}
 		return condition;
 	}
